f0aebb9623c03837d4aa8107265ab834b24e37f7
Muhammad Azeem <azeemrauf7474@gmail.com>
2024-11-23 22:54:26 +0500

    code update

--- src/components/Experience/mywork_data.jsx
+++ src/components/Experience/mywork_data.jsx
@@ -4,40 +4,40 @@ const mywork_data = [
         w_title:"Movies Website",
         w_desc:'Developed a movie search website using the OMDb API, allowing users to explore and discover movie details effortlessly.',
         w_gitUrl:'',
-        w_img:'assets/project_1.svg'
+        w_img:'assets/movie_project.png'
 
     },
     {
         w_title:"Weather Web APP",
         w_desc:'Created a weather application using React and Tailwind CSS, providing users with real-time weather updates and forecasts based on their location.',
         w_gitUrl:'',
-         w_img:'assets/project_2.svg'
+         w_img:'assets/weather_project.png'
     },
     {
         w_title:"To Do Web App",
         w_desc:'Built a dynamic to-do app with React js for efficient task management, featuring a seamless, interactive interface.',
         w_gitUrl:'',
-        w_img:'assets/project_3.svg'
+        w_img:'assets/todo_project.png'
     },
     {
         w_title:"Portfolio Website",
         w_desc:'Developed a sleek portfolio website showcasing projects, skills, and experience with a clean, responsive design.',
         w_gitUrl:'',
-        w_img:'assets/project_4.svg'
+        w_img:'assets/portfolio_project.png'
     },
     {
         w_title:"Youtube to Mp3 Converter",
         w_desc:'Created an efficient YouTube to MP3 converter with a user-friendly interface, offering quick and reliable conversions with a sleek design.',
         w_gitUrl:'',
-        w_img:'assets/project_5.svg'
+        w_img:'assets/mp3converter_project.png'
     },
-    {
-        w_title:"Web design",
-        w_desc:'',
-        w_gitUrl:'',
-        w_img:'assets/project_6.svg'
+    // {
+    //     w_title:"Web design",
+    //     w_desc:'',
+    //     w_gitUrl:'',
+    //     w_img:'assets/project_6.svg'
 
-    },
+    // },
 ]
  
 export default mywork_data;
